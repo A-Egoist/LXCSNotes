3be2864e5f2805b3cf45f2d278cac1b44af49029
a-egoist <1012738513@qq.com>
2025-07-23 11:18:48 +0800

    update

--- site/CPP/src/1.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-
-int main() {
-    std::cout << sizeof(std::string) << std::endl;
-    
-    return 0;
-}
--- site/CPP/src/Basic/MacroDefinition01.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#define LOG(o, ...) fprintf(o, "[%s:%d]", __FILE__, __LINE__); fprintf(o, __VA_ARGS__)
-
-int main() {
-    LOG(stdout, "%s\n", "测试信息, 日志001");
-    // fprintf(__stdoutp, "[%s:%d]", "/Users/amonologue/Project/Notes/CPP/src/Basic/MacroDefinition.cpp", 6);
-    // fprintf(__stdoutp, "%s\n", "测试信息, 日志001");
-}
